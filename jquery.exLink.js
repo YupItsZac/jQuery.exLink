450bfcb0f7e60d253f8cdaeef5f22ff5fc78655e
= <=>
2015-03-29 15:19:49 -0700

    Corrected logical flaw that caused a single link to be opened twice.

--- jquery.exLink.js
+++ jquery.exLink.js
@@ -11,113 +11,132 @@
 
     $.fn.exLink = function(options) {
 
-    	var defaults = {
-    		protocols: ['http', 'https'],
-    		filetypes: ['pdf', 'xls', 'docx', 'doc', 'ppt', 'pptx'],
-    		linkWarning: true,
-    		linkWarningBody: 'You are about to leave this website and navigate to the link below. Would you like to continue?',
-    		fileWarning: true,
-    		fileWarningBody: 'You are about to open the file below. Do you wish to continue?',
-    		dialogConfirm: '#006600',
-    		dialogCancel: '#CC0000',
-    		dialogConfirmText: '#fff',
-    		dialogCancelText: '#fff',
-    		dialogCancelButton: 'Cancel',
-    		dialogConfirmButton: 'Continue',
-    		modalWidth: "320px",
-			modalHeight: "240px"
-    		
-    	}
+        var defaults = {
+            protocols: ['http', 'https'],
+            filetypes: ['pdf', 'xls', 'docx', 'doc', 'ppt', 'pptx'],
+            linkWarning: true,
+            linkWarningBody: 'You are about to leave this website and navigate to the link below. Would you like to continue?',
+            fileWarning: true,
+            fileWarningBody: 'You are about to open the file below. Do you wish to continue?',
+            dialogConfirm: '#006600',
+            dialogCancel: '#CC0000',
+            dialogConfirmText: '#fff',
+            dialogCancelText: '#fff',
+            dialogCancelButton: 'Cancel',
+            dialogConfirmButton: 'Continue',
+            modalWidth: "320px",
+            modalHeight: "240px"
+            
+        }
 
 
         if(options != 're') {
-    	   jQuery.options = $.extend({}, defaults, options);
-        }	
+           jQuery.options = $.extend({}, defaults, options);
+        }   
 
-    	var self = this;
+        var self = this;
 
-    	targetLinks();
+        targetLinks();
 
-    	$( ".exLink, .docuLink" ).click(function( event ) {
-			event.preventDefault();
+        $( ".exLink, .docuLink" ).click(function( event ) {
+            event.preventDefault();
 
-			catchClick(event);
-		});
+            catchClick(event);
+        });
  
     },
 
     targetLinks = function() {
 
-    	var self = this;
+        var self = this;
 
-    	jQuery.each(jQuery.options.protocols, function(key, value) {
-			$('a[href^="'+value+'://"]').addClass("exLink");
-    	});
+        jQuery.each(jQuery.options.protocols, function(key, value) {
+            $('a[href^="'+value+'://"]').addClass("exLink");
+        });
 
-    	jQuery.each(jQuery.options.filetypes, function(key, value) {
-    		$('a[href$="'+value+'"]').addClass('docuLink');
-    	});
+        jQuery.each(jQuery.options.filetypes, function(key, value) {
+            $('a[href$="'+value+'"]').addClass('docuLink');
+        });
     },
 
     catchClick = function(obj) {
-		
-		if($(obj.target).is('.exLink')) {
-			if(jQuery.options.linkWarning) {
-				showLinkWarning(obj.target.href);
-			} else {
-				window.open(obj.target.closest('a').href, '_blank');
-			}
-		} else {
-			if(jQuery.options.fileWarning) {
-				showDocWarning(obj.target.href);
-			} else {
-				window.open(obj.target.closest('a').href, '_blank');
-			}
-		}
-	},
 
-	showLinkWarning = function(href) {
-    	$('body').append('<div class="modalBG"></div>');
-		$('.modalBG').fadeIn("slow");
-		$('body').append('<div class="modal-dialog">'+jQuery.options.linkWarningBody+'<br><br><p><center><b>'+href+'</b></center></p><br><br><div class="exLinkButton exLinkCancel" onclick="$.fn.exLink.closeModal();">'+jQuery.options.dialogCancelButton+'</div><div class="exLinkButton exLinkContinue" onclick="$.fn.exLink.navigateLocation(&quot;'+href+'&quot;);">'+jQuery.options.dialogConfirmButton+'</div></div>');
-		$('.modal-dialog').fadeIn("slow");
+        if(window.obj == obj.target) {
+            //do nothing. this event has already been fired
 
-		$('.exLinkCancel').css("background-color",jQuery.options.dialogCancel);
-		$('.exLinkContinue').css('background-color', jQuery.options.dialogConfirm);
-		$('.exLinkCancel').css('color', jQuery.options.dialogCancelText);
-		$('.exLinkContinue').css('color', jQuery.options.dialogConfirmText);
+        } else {
 
-		$('.modal-dialog').css('width', jQuery.options.modalWidth);
-		$('.modal-dialog').css('height', jQuery.options.modalHeight);
+            window.obj = obj.target;
+        
+            if($(obj.target).is('.exLink')) {
+                if(jQuery.options.linkWarning) {
+                    showLinkWarning(obj.target.href);
+                } else {
+                    if($(obj.target).is('a')) {
+                        var href = obj.target.href;
+                    } else {
+                        var href = obj.target.closest('a').href;
+                    }
+                    window.open(href, '_blank');
+                }
+            } else {
+                if(jQuery.options.fileWarning) {
+                    showDocWarning(obj.target.href);
+                } else {
+                    if($(obj.target).is('a')) {
+                        var href = obj.target.href;
+                    } else {
+                        var href = obj.target.closest('a').href;
+                    }
+
+                    window.open(href, '_blank');
+                }
+            }
+        }
+    },
+
+    showLinkWarning = function(href) {
+        $('body').append('<div class="modalBG"></div>');
+        $('.modalBG').fadeIn("slow");
+        $('body').append('<div class="modal-dialog">'+jQuery.options.linkWarningBody+'<br><br><p><center><b>'+href+'</b></center></p><br><br><div class="exLinkButton exLinkCancel" onclick="$.fn.exLink.closeModal();">'+jQuery.options.dialogCancelButton+'</div><div class="exLinkButton exLinkContinue" onclick="$.fn.exLink.navigateLocation(&quot;'+href+'&quot;);">'+jQuery.options.dialogConfirmButton+'</div></div>');
+        $('.modal-dialog').fadeIn("slow");
+
+        $('.exLinkCancel').css("background-color",jQuery.options.dialogCancel);
+        $('.exLinkContinue').css('background-color', jQuery.options.dialogConfirm);
+        $('.exLinkCancel').css('color', jQuery.options.dialogCancelText);
+        $('.exLinkContinue').css('color', jQuery.options.dialogConfirmText);
+
+        $('.modal-dialog').css('width', jQuery.options.modalWidth);
+        $('.modal-dialog').css('height', jQuery.options.modalHeight);
     },
 
     showDocWarning = function(href) {
-    	$('body').append('<div class="modalBG"></div>');
-		$('.modalBG').fadeIn("slow");
-		$('body').append('<div class="modal-dialog">'+jQuery.options.fileWarningBody+'<br><br><p><center><b>'+href+'</b></center></p><br><br><div class="exLinkButton exLinkCancel" onclick="$.fn.exLink.closeModal();">'+jQuery.options.dialogCancelButton+'</div><div class="exLinkButton exLinkContinue" onclick="$.fn.exLink.navigateLocation(&quot;'+href+'&quot;);">'+jQuery.options.dialogConfirmButton+'</div></div>');
-		$('.modal-dialog').fadeIn("slow");
+        $('body').append('<div class="modalBG"></div>');
+        $('.modalBG').fadeIn("slow");
+        $('body').append('<div class="modal-dialog">'+jQuery.options.fileWarningBody+'<br><br><p><center><b>'+href+'</b></center></p><br><br><div class="exLinkButton exLinkCancel" onclick="$.fn.exLink.closeModal();">'+jQuery.options.dialogCancelButton+'</div><div class="exLinkButton exLinkContinue" onclick="$.fn.exLink.navigateLocation(&quot;'+href+'&quot;);">'+jQuery.options.dialogConfirmButton+'</div></div>');
+        $('.modal-dialog').fadeIn("slow");
 
-		$('.exLinkCancel').css("background-color",jQuery.options.dialogCancel);
-		$('.exLinkContinue').css('background-color', jQuery.options.dialogConfirm);
-		$('.exLinkCancel').css('color', jQuery.options.dialogCancelText);
-		$('.exLinkContinue').css('color', jQuery.options.dialogConfirmText);
+        $('.exLinkCancel').css("background-color",jQuery.options.dialogCancel);
+        $('.exLinkContinue').css('background-color', jQuery.options.dialogConfirm);
+        $('.exLinkCancel').css('color', jQuery.options.dialogCancelText);
+        $('.exLinkContinue').css('color', jQuery.options.dialogConfirmText);
 
-		$('.modal-dialog').css('width', jQuery.options.modalWidth);
-		$('.modal-dialog').css('height', jQuery.options.modalHeight);
+        $('.modal-dialog').css('width', jQuery.options.modalWidth);
+        $('.modal-dialog').css('height', jQuery.options.modalHeight);
     };
 
     $.fn.exLink.closeModal = function() {
 
-    	$('.modalBG').remove();
-    	$('.modal-dialog').remove();
+        $('.modalBG').remove();
+        $('.modal-dialog').remove();
     };
 
     $.fn.exLink.navigateLocation = function(href) {
 
-    	window.open(href, '_blank');
+        window.open(href, '_blank');
 
-		$('.modalBG').remove();
-    	$('.modal-dialog').remove();  
-    };  	
+        $('.modalBG').remove();
+        $('.modal-dialog').remove();  
+    };      
  
 })( jQuery );
